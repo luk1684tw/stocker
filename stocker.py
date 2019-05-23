d01c773b089324b3b12eb7bb25f9c1dd7db297c7
luk1684tw <luk1684tw@gmail.com>
2019-05-23 23:20:41 +0800

    modify predict_future to accept label and plot

--- stocker.py
+++ stocker.py
@@ -873,6 +873,7 @@ class Stocker():
         # Plot the estimates
         ax.plot(future_increase['Date'], future_increase['estimate'], 'g^', ms = 12, label = 'Pred. Increase')
         ax.plot(future_decrease['Date'], future_decrease['estimate'], 'rv', ms = 12, label = 'Pred. Decrease')
+        ax.plot(label['Date'], label['Price'], 'ko-' , ms = 12, label = 'Truth')
 
         # Plot errorbars
         ax.errorbar(future['Date'].dt.to_pydatetime(), future['estimate'], 
