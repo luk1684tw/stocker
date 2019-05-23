454b8239d2932037d3016dfd7145db41e15fea81
luk1684tw <luk1684tw@gmail.com>
2019-05-23 14:36:13 +0800

    print stock history dataframe

--- stocker.py
+++ stocker.py
@@ -435,7 +435,7 @@ class Stocker():
         if resample:
             stock_history = self.resample(stock_history)
         
-        print ('history:',stock_history)
+        print ('history:',stock_history['Date'].dype)
         model.fit(stock_history)
         
         # Make and predict for next year with future dataframe
