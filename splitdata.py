4dc010d4afd76be5997967e4251f74e05a1f23ab
luk1684tw <luk1684tw@gmail.com>
2019-06-09 03:05:09 +0800

    standardlize training data

--- splitdata.py
+++ splitdata.py
@@ -44,9 +44,9 @@ googleData = googleData.drop('Stock', axis=1)
 amazData = amazData.drop('Stock', axis=1)
 fbData = fbData.drop('Stock', axis=1)
 
-googleData = googleData[googleData['Date'] < pd.to_datetime('2018-1-1')]
-amazData = amazData[amazData['Date'] < pd.to_datetime('2018-1-1')]
-fbData = fbData[fbData['Date'] < pd.to_datetime('2018-1-1')]
+googleData = googleData[googleData['Date'] < pd.to_datetime('2018-1-2')]
+amazData = amazData[amazData['Date'] < pd.to_datetime('2018-1-2')]
+fbData = fbData[fbData['Date'] < pd.to_datetime('2018-1-2')]
 
 scaler = StandardScaler()
 googleData = googleData.values
